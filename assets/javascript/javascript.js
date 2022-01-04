d9f5ff575204232cbc59d8dd61bcc6b4e46a9fa7
Juno <junonguyen27@gmail.com>
2022-01-04 17:51:22 -0600

    working on 5 day forecast

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,8 +3,10 @@ var findCity = document.querySelector("#findcity");
 var APIKey = "e3fc80ad5571c3e2bbb3009c402430e0";
 var tempEl = $("#temp");
 var windEl = $("#wind");
-var humidityEl = $("humidity");
+var humidityEl = $("#humidity");
 var indexEl = $("#index");
+var forecastDailyEl = $("#forecastdaily");
+
 
 var getCityWeather = function(city){
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+findCity.value+'&appid='+APIKey;
@@ -22,7 +24,7 @@ var getCityWeather = function(city){
                     console.log(data.coord.lon);
                     var lat = data.coord.lat;
                     var lon = data.coord.lon;
-                    var apiUrl2 = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&appid='+APIKey;
+                    var apiUrl2 = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&units=imperial&appid='+APIKey;
                     console.log(apiUrl2);
 
                     fetch(apiUrl2)
@@ -31,10 +33,51 @@ var getCityWeather = function(city){
                                 console.log(response);
                                 response.json().then(function(data){
                                     console.log(data);
-                                    humidityEl.textContent = data.current.humidity;
-                                    console.log(data.current.humidity);
-                                    console.log(tempEl.textContent);
-                                })
+
+                                    tempData = document.createElement('p');
+                                    tempData.textContent = data.current.temp + "°F";
+                                    tempEl.append(tempData);
+
+                                    windData = document.createElement('p');
+                                    windData.textContent = data.current.wind_gust + "MPH";
+                                    windEl.append(windData);
+
+                                    humidityData = document.createElement('p');
+                                    humidityData.textContent = data.current.humidity + "%";
+                                    humidityEl.append(humidityData);
+
+                                    indexData = document.createElement('p');
+                                    indexData.textContent = data.current.uvi;
+                                    indexEl.append(indexData);
+
+                                    for (var i = 0; i < 5; i++) {
+                                        var dailyBox = document.createElement('div');
+                                        var dailyTemp = document.createElement('p');
+                                        var dailyWind = document.createElement('p');
+                                        var dailyHumidity = document.createElement('p');
+
+                                        dailyTemp.textContent = data.daily[i].temp.day + "°F";
+                                        dailyWind.textContent = data.daily[i].wind_gust + "MPH";
+                                        dailyHumidity.textContent = data.daily[i].humidity + "%";
+
+                                        forecastDailyEl.dailyBox.append(dailyTemp);
+                                        forecastDailyEl.dailyBox.append(dailyWind);
+                                        forecastDailyEl.dailyBox.append(dailyHumidity);
+                                    };
+                                });
+                                    // for (var i = 0; i < 4; i++) {
+                                    //     var dailyTemp = document.createElement('p');
+                                    //     var dailyWind = document.createElement('p');
+                                    //     var dailyHumidity = document.createElement('p');
+
+                                    //     dailyTemp.textContent = data.daily[i].temp + "°F";
+                                    //     dailyWind.textContent = data.daily[i].wind_gust + "MPH";
+                                    //     dailyHumidity.textContent = data.daily[i].humidity + "%";
+
+                                    //     forecastDailyEl.append(dailyTemp);
+                                    //     forecastDailyEl.append(dailyWind);
+                                    //     forecastDailyEl.append(dailyHumidity);
+                                    // };
                             }
                         })
                             
